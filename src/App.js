9a151e40a8f29f8e1039530485679cb890b02978
Diegobr21 <jdiegobrana@gmail.com>
2021-08-24 23:24:11 -0500

    using api key

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ class App extends Component{
   }
 
   async componentDidMount(){
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
     this.setState({loading: true})
 
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({users: res.data, loading:false})
   }
@@ -23,7 +25,6 @@ class App extends Component{
 
     return (
      <Fragment> 
-        <h1>Hello dev!</h1>
         <Navbar></Navbar>
         <div className='container'>
           <Users loading={this.state.loading} users={this.state.users}></Users>
