d854a049949045ccc79bf1b2a035a02378bbf9e2
Diegobr21 <jdiegobrana@gmail.com>
2021-08-24 22:31:41 -0500

    fetching all possible users

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 import React , { Component, Fragment } from  'react' ;
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component{
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount(){
+    this.setState({loading: true})
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({users: res.data, loading:false})
+  }
   render(){
 
     return (
@@ -12,7 +26,7 @@ class App extends Component{
         <h1>Hello dev!</h1>
         <Navbar></Navbar>
         <div className='container'>
-          <Users></Users>
+          <Users loading={this.state.loading} users={this.state.users}></Users>
         </div>
         
       </Fragment>
