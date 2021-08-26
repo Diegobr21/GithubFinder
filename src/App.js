562253eb10eb88720cddef4e6922211caf7db03f
Diegobr21 <jdiegobrana@gmail.com>
2021-08-25 23:14:23 -0500

    basic clear users

--- src/App.js
+++ src/App.js
@@ -24,13 +24,16 @@ class App extends Component{
     this.setState({users: res.data.items, loading:false})
   }
 
+  //Clear users from state
+  clearUsers = () => this.setState({users: [], loading:false})
+
   render(){
 
     return (
      <Fragment> 
         <Navbar></Navbar>
         <div className='container'>
-          <Search searchUsers={this.searchUsers}/>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers}/>
           <Users loading={this.state.loading} users={this.state.users}></Users>
         </div>
         
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 export class Search extends Component {
     state = {
         text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired
     }
 
     onChange = (e) => this.setState({[e.target.name]: e.target.value});
@@ -26,6 +33,7 @@ export class Search extends Component {
                     className='btn btn-dark btn-block'
                     value='Search' />
                 </form>
+                <button className='btn btn-light btn-block' onClick={this.props.clearUsers} >Clear</button>
             </div>
         )
     }
